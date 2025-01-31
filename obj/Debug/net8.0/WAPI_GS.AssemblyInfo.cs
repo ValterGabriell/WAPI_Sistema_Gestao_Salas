84879f770ab35549b6fffb7489d8c54d24cb1360
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-01-31 10:03:09 -0300

    update

--- obj/Debug/net8.0/WAPI_GS.AssemblyInfo.cs
+++ obj/Debug/net8.0/WAPI_GS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WAPI_GS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db414f5e9f9923dddf56d87baf482e972362546d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c90eeadb6ac1efb7567ca37f02d698330777e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WAPI_GS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WAPI_GS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
