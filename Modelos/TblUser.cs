6edb64702a2f4585bbc1de5a4eb673ae93883f30
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-02-20 22:13:18 -0300

    update resopnse

--- Modelos/TblUser.cs
+++ Modelos/TblUser.cs
@@ -26,4 +26,7 @@ public class TblUser
     public string? Username { get; set; }
     [Column("password")]
     public string? Password { get; set; }
+
+    [Column("color")]
+    public string? Color { get; set; }
 }
--- obj/Debug/net8.0/WAPI_GS.AssemblyInfo.cs
+++ obj/Debug/net8.0/WAPI_GS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WAPI_GS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78ddb72a1cbaa17c801051a2df952f79f73361df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5e30565769e422921f397ae649eb9270d018045")]
 [assembly: System.Reflection.AssemblyProductAttribute("WAPI_GS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WAPI_GS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
