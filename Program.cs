8a5d1614cce04429c435e2fd66c86c483cbd98e7
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-07-28 21:55:00 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -52,11 +52,8 @@ builder.Services.AddScoped<IUnitOfWork, UOWService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
